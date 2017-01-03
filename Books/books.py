abf8c3ad771ec786f55e9b06dab5be2f0640fd18
Anand Swaminathan <aswamina@gmail.com>
2017-01-03 12:30:38 -0600

    Initial commit. I am going to incorporate a new Bootstrap theme shortly

--- Books/books.py
+++ Books/books.py
@@ -1,7 +1,9 @@
 from flask import Flask, json, render_template, url_for, Response
+from flask_cors import CORS, cross_origin
 
 # Initialize the Flask application
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
