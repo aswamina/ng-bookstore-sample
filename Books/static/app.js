b0ff999fbf40fe522705f14da93ffc92cb4919e6
Anand Swaminathan <aswamina@gmail.com>
2017-01-06 22:20:05 -0600

    remove panel controller code which has been moved to its bookPanelCtrl.js file

--- Books/static/app.js
+++ Books/static/app.js
@@ -1,18 +1,6 @@
 (function() {
     var app = angular.module('bookStore', []);
 
-    app.controller('PanelController', function() {
-        this.tab = 1;
-
-        this.selectTab = function(setTab) {
-            this.tab = setTab;
-        }
-
-        this.isSelected = function(checkTab) {
-            return this.tab === checkTab;
-        }
-    });
-
     app.controller('ReviewController', function() {
         this.review = {};
 
