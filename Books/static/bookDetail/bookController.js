bb16dbbf50400e6234321a08f1a7baf4e83f8ece
Anand Swaminathan <aswamina@gmail.com>
2017-01-06 19:08:09 -0600

    changes to get carousel to work. Also, commented out carousel event handling. its not working. will need to figure this out next.

--- Books/static/bookDetail/bookController.js
+++ Books/static/bookDetail/bookController.js
@@ -4,15 +4,18 @@
         $http.get('http://127.0.0.1:5000/getBooks').success(function(data) {
             $scope.books = data;
         });
-
+/*
         $('#carousel-of-books').on('slide.bs.carousel', function (event) {
-            var active = $(event.target).find('.carousel-inner > .item.active');
-            var from = active.index();
-            var next = $(event.relatedTarget);
-            var to = next.index();
-            var direction = event.direction;
+                console.log('slide event!');
 
-            $scope.selectedBook = $scope.books[to].name;
+                var active = $(event.target).find('.carousel-inner > .item.active');
+                var from = active.index();
+                var next = $(event.relatedTarget);
+                var to = next.index();
+                var direction = event.direction;
+
+                $scope.selectedBook = $scope.books[to].name;
         });
+*/
     }]);
 })();
