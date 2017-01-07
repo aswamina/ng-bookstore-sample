fa7e43dfb52293af4b80fd24c32c6085ab5550cf
Anand Swaminathan <aswamina@gmail.com>
2017-01-06 22:16:09 -0600

    added link function to add the selected book to scope on slide transition

--- Books/static/bookCarousel/bookCarousel.js
+++ Books/static/bookCarousel/bookCarousel.js
@@ -2,7 +2,20 @@
     angular.module('bookStore').directive('bookCarousel', function () {
         return {
             restrict: 'E',
-            templateUrl: 'static/bookCarousel/bookCarousel.html'
+            templateUrl: 'static/bookCarousel/bookCarousel.html',
+            link : function(scope, element, attrs) {
+                //Add event listener for 'slide' event
+                element.on('slide.bs.carousel', function(event) {
+                    var active = $(event.target).find('.carousel-inner > .item.active');
+                    var from = active.index();
+                    var next = $(event.relatedTarget);
+                    var to = next.index();
+                    var direction = event.direction;
+
+                    scope.selectedBook = scope.books[to].name;
+                    console.log("selectedBook", scope.selectedBook);
+                });
+            }
         };
     });
 })();
