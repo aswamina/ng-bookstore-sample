5ef7006cab5ea5324a1f8c7e5a8df42cc379555d
Anand Swaminathan <aswamina@gmail.com>
2017-01-06 22:17:39 -0600

    removed commented out carousel slide event handler. This handler has been moved to the link function in bookController.js

--- Books/static/bookDetail/bookController.js
+++ Books/static/bookDetail/bookController.js
@@ -4,18 +4,5 @@
         $http.get('http://127.0.0.1:5000/getBooks').success(function(data) {
             $scope.books = data;
         });
-/*
-        $('#carousel-of-books').on('slide.bs.carousel', function (event) {
-                console.log('slide event!');
-
-                var active = $(event.target).find('.carousel-inner > .item.active');
-                var from = active.index();
-                var next = $(event.relatedTarget);
-                var to = next.index();
-                var direction = event.direction;
-
-                $scope.selectedBook = $scope.books[to].name;
-        });
-*/
     }]);
 })();
