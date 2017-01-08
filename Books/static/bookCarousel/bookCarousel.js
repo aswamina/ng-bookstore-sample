6e31528556e262fed66609934c8a5be524ac89d7
Anand Swaminathan <aswamina@gmail.com>
2017-01-07 19:10:54 -0600

    added event handlers in link function to pause carousel and updated the book selection for book panel. Also added mouseleave function to resume carousel slide and on clicking the carousel indicators

--- Books/static/bookCarousel/bookCarousel.js
+++ Books/static/bookCarousel/bookCarousel.js
@@ -6,14 +6,31 @@
             link : function(scope, element, attrs) {
                 //Add event listener for 'slide' event
                 element.on('slide.bs.carousel', function(event) {
+
                     var active = $(event.target).find('.carousel-inner > .item.active');
                     var from = active.index();
                     var next = $(event.relatedTarget);
                     var to = next.index();
                     var direction = event.direction;
 
+                    scope.selectedBookIndex = to;
                     scope.selectedBook = scope.books[to];
                     console.log("selectedBook", scope.selectedBook.name);
+                });
+
+                $("#carousel-of-books").on('click', function(event) {
+                    scope.activateBookPanel = true;
+                    console.log("activateBookPanel=", scope.activateBookPanel);
+                });
+
+                $("#carousel-of-books").on('mouseleave', function(event) {
+                    scope.activateBookPanel = false;
+                    console.log("activateBookPanel=", scope.activateBookPanel);
+                });
+
+                $(".carousel-indicators").on('click', function(event) {
+                    scope.activateBookPanel = false;
+                    console.log("activateBookPanel=", scope.activateBookPanel);
                 });
             }
         };
