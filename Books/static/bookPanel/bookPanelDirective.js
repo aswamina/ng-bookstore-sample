6ac3deb13114b2a6a50e1d99191da663af0906e8
Anand Swaminathan <aswamina@gmail.com>
2017-01-07 19:06:26 -0600

    added link function to pause cancel when book panel tabs are clicked. Also if carousel has already slid, it will scroll to show the selected book image

--- Books/static/bookPanel/bookPanelDirective.js
+++ Books/static/bookPanel/bookPanelDirective.js
@@ -2,9 +2,17 @@
     angular.module('bookStore').directive('bookPanel', function () {
         return {
             restrict: 'E',
-            templateUrl: 'static/bookPanel/bookPanel.html'
-            // CREATE A LINK FUNCTION HERE THAT IMPLEMENTS THE FOLLOWING CODE
-            // $("#myCarousel").pause()
+            templateUrl: 'static/bookPanel/bookPanel.html',
+            link: function(scope, element, attrs) {
+                // CREATE A LINK FUNCTION HERE THAT IMPLEMENTS THE FOLLOWING CODE
+                // $("#myCarousel").pause()
+                element.on('click', function() {
+                    $("#carousel-of-books").carousel(scope.selectedBookIndex);
+                    $("#carousel-of-books").carousel('pause');
+                    scope.activateBookPanel = true;
+                    console.log("paneldirective: activateBookPanel", scope.activateBookPanel);
+                });
+            }
         };
     });
 })();
