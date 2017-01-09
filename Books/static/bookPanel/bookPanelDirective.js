859c16748ef6d61e1791a28c46a795ec287279f5
Anand Swaminathan <aswamina@gmail.com>
2017-01-09 07:25:53 -0600

    remove the activatebookpanel status element ... replaced by methods that listen to events

--- Books/static/bookPanel/bookPanelDirective.js
+++ Books/static/bookPanel/bookPanelDirective.js
@@ -6,11 +6,10 @@
             link: function(scope, element, attrs) {
                 // CREATE A LINK FUNCTION HERE THAT IMPLEMENTS THE FOLLOWING CODE
                 // $("#myCarousel").pause()
+
                 element.on('click', function() {
                     $("#carousel-of-books").carousel(scope.selectedBookIndex);
                     $("#carousel-of-books").carousel('pause');
-                    scope.activateBookPanel = true;
-                    console.log("paneldirective: activateBookPanel", scope.activateBookPanel);
                 });
             }
         };
