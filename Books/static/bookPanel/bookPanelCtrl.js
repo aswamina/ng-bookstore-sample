9c2c9610ce65feba455ddab0d4f4bcf5c93a5026
Anand Swaminathan <aswamina@gmail.com>
2017-01-09 12:27:53 -0600

    added methods to set book panel status. the app works except on mouseleave when the book panel status is not updated on scope. checking in before fixing that issue.

--- Books/static/bookPanel/bookPanelCtrl.js
+++ Books/static/bookPanel/bookPanelCtrl.js
@@ -1,27 +1,36 @@
 (function() {
     angular.module('bookStore').controller('bookPanelCtrl', ['$scope', function($scope) {
         console.log("in panel controller");
-        $scope.tab = 0;
-
+        //$scope.tab = 0;
+        $scope.setBookPanelStatus = function(status) {
+            $scope.openBookPanel = {
+                state: status
+            };
+            console.log("status area, scope=", $scope);
+        };
 
         $scope.selectTab = function(setTab) {
             $scope.tab = setTab;
+            $scope.setBookPanelStatus(true);
             console.log("tab = ", $scope.tab);
         };
 
         $scope.isTabSelected = function(checkTab) {
             return $scope.tab === checkTab;
         };
-/*
+
+
         $scope.$on("bookPanel:expand", function(event) {
-            console.log("HERE expanding BookPanel");
-            $scope.selectTab(1);
+            console.log("Received event to expand book panel");
+            $scope.setBookPanelStatus(true);
         });
 
         $scope.$on("bookPanel:collapse", function(event) {
-            console.log("GOAT collapsing book panel");
+            console.log("Received event to collapse book panel");
+            $scope.setBookPanelStatus(false);
             $scope.selectTab(0);
         });
-*/
+
+        $scope.setBookPanelStatus(false);
     }]);
 })();
