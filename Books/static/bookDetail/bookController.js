016a1546e6a02dbd156648c7ca586dcdd8424fd0
Anand Swaminathan <aswamina@gmail.com>
2017-01-09 07:02:08 -0600

    added methods to subscribe to events for expanding and collapsing book panel based on the carousel actions

--- Books/static/bookDetail/bookController.js
+++ Books/static/bookDetail/bookController.js
@@ -1,8 +1,16 @@
 (function() {
-    angular.module('bookStore').controller('StoreCtrl', ['$scope','$http', function($scope, $http) {
+    angular.module('bookStore').controller('StoreCtrl', ['$scope','$http', '$rootScope', function($scope, $http, $rootScope) {
 
         $http.get('http://127.0.0.1:5000/getBooks').success(function(data) {
             $scope.books = data;
         });
+
+        $scope.expandBookPanel = function() {
+            $rootScope.$broadcast("bookPanel:expand");
+        };
+
+        $scope.collapseBookPanel = function() {
+            $rootScope.$broadcast("bookPanel:collapse");
+        };
     }]);
 })();
