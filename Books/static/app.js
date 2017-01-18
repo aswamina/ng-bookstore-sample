77747156f944dced03b2b42786cab7a414003b64
Anand Swaminathan <aswamina@gmail.com>
2017-01-18 07:45:18 -0600

    moved reviewcontroller from this file into its own module. checking in because reviews are now in their own files and works at the same level as before. next step... amazon style reviews.

--- Books/static/app.js
+++ Books/static/app.js
@@ -1,13 +1,3 @@
 (function() {
     var app = angular.module('bookStore', []);
-    /*
-    app.controller('ReviewController', function() {
-        this.review = {};
-
-        this.addReview = function(product) {
-            product.reviews.push(this.review);
-            this.review = {};
-        }
-    });
-    */
 })();
